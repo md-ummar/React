fcd6eddce2ef56cc529cbba436a583833ccff437
md ummar <mdummar.28@gmail.com>
2024-04-22 14:42:50 +0530

    ..

--- 08ThemeSwitcher/src/context/ThemeContext.js
+++ 08ThemeSwitcher/src/context/ThemeContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, Provider, useContext } from "react";
+import { createContext, useContext } from "react";
 
 export const ThemeContext = createContext({
   themeMode: "dark",
